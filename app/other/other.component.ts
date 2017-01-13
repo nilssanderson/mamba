00f2c6e4af7244ce4bd9e025a11cad870ecee6e2
Nils Sanderson <thebaldwan@gmail.com>
2017-01-13 15:44:52 +0000

    Updating paths for template and styles

--- app/other/other.component.ts
+++ app/other/other.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'fa-other',
-    templateUrl: './other.component.html'
+    templateUrl: 'app/other/other.component.html'
 })
 
 export class OtherComponent implements OnInit {
