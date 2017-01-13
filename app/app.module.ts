ac39bc7806d4a4037af80384fa131ffb2e652995
Nils Sanderson <thebaldwan@gmail.com>
2017-01-13 15:29:33 +0000

    Added another compontent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,22 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { OtherComponent }  from './other/other.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    OtherComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
